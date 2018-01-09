346898bba8f54c71e5164d6f0f2b2d5b69c1f578
Niprichem <vbolshakoff@yandex.ru>
2018-01-10 02:33:07 +0600

    1

--- thesis.py
+++ thesis.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 def get_user_groups(params):
@@ -6,14 +7,29 @@ def get_user_groups(params):
     return response
 
 
+def get_group_by_id(id_, version):
+    params = {
+        'group_id': id_,
+        'fields': ['members_count'],
+        'v': version
+    }
+    response = requests.get('https://api.vk.com/method/groups.getById', params).json()
+    group_data = response['response'][0]
+    return {
+        'name': group_data['name'],
+        'gid': group_data['id'],
+        'members_count': group_data['members_count']
+    }
+
+
 def main():
-    VERSION = '5,69'
-    TOKEN = '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128'
+    version = '5,69'
+    token = '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128'
 
     params = {
         'user_id': 5030613,
-        'access_token': TOKEN,
-        'v': VERSION
+        'access_token': token,
+        'v': version
     }
 
     response = requests.get('https://api.vk.com/method/friends.get', params).json()
@@ -23,11 +39,11 @@ def main():
     groups = set(group_response['response']['items'])
 
     all_friend_groups = set()
-    for friend_id in friends:
+    for i, friend_id in enumerate(friends):
         params = {
             'user_id': friend_id,
-            'access_token': TOKEN,
-            'v': VERSION
+            'access_token': token,
+            'v': version
         }
 
         response = get_user_groups(params)
@@ -35,10 +51,17 @@ def main():
             continue
 
         all_friend_groups = all_friend_groups | set(response['response']['items'])
-        print('*******************************************************')
-        #print(set(response['response']['items']))
+        print(i)
 
-    print('result: {}'.format(groups - all_friend_groups))
+    group_id_set = groups - all_friend_groups
+
+    result_list = []
+    for group_id in group_id_set:
+        result_list.append(get_group_by_id(group_id, version))
+
+    filename = 'groups.json'
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(result_list, file, indent=4)
 
 
 if __name__ == '__main__':
